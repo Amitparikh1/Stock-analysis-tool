d4597a0d1e8cee2265f7c2810a0253ad1e305dd5
Amit Parikh <amitparikh41@gmail.com>
2020-05-28 13:12:05 -0400

    button created and grid system used

--- GUI.py
+++ GUI.py
@@ -1,1 +1,18 @@
 ##GUI
+### Imports ###
+from tkinter import *
+
+### Basic Setup ###
+root = Tk() #create root window to put widgets onto
+#labels
+title = Label(root,text="Stock Analyzer")
+instructions = Label(root,text="Pick a stock")
+#buttons
+submitButton = Button(root,text="Submit",state=NORMAL,bg="green",fg="white")
+
+### Put on Screen ###
+title.grid(row=0,column=0)
+instructions.grid(row=1,column=1)
+submitButton.grid(row=1,column=2)
+### Main loop ###
+root.mainloop()
