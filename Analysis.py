b339c453422e752e1f70c53e06cfc81f987fdf65
Amit Parikh <amitparikh41@gmail.com>
2020-05-29 14:55:12 -0400

    scraping stock data from yahoo finance using beautiful soup

--- Analysis.py
+++ Analysis.py
@@ -1,1 +1,23 @@
 ##Analysis
+### Imports ###
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import pandas as pd
+import matplotlib.pyplot as plt
+
+## Scrape Stock Data
+def scrapeData(ticker):
+    url = "https://finance.yahoo.com/quote/"+ticker+"?p="+ticker+"&.tsrc=fin-srch" #find the stock on yahoo finance
+    try:
+        page = urlopen(url)
+    except:
+        print("Error opening url")
+    
+    soup = BeautifulSoup(page,"html.parser")
+    return soup
+
+def getCurrentPrice(soup):
+    currentPrice = soup.find('div',{'class': 'My(6px) Pos(r) smartphone_Mt(6px)'}).find('span').text #find the stock price from the website
+    return currentPrice
+
+print(getCurrentPrice(scrapeData("AAPL")))
--- GUI.py
+++ GUI.py
@@ -2,17 +2,21 @@
 ### Imports ###
 from tkinter import *
 
-### Basic Setup ###
+### Basic Setup of Widgets ###
 root = Tk() #create root window to put widgets onto
 #labels
 title = Label(root,text="Stock Analyzer")
-instructions = Label(root,text="Pick a stock")
+instructions = Label(root,text="Stock 1:")
 #buttons
 submitButton = Button(root,text="Submit",state=NORMAL,bg="green",fg="white")
-
+#input box
+inputStock = Entry(root) # use .get() to get the text from the input box
+inputStock.insert(0,"Ticker Symbol")
 ### Put on Screen ###
-title.grid(row=0,column=0)
-instructions.grid(row=1,column=1)
+title.grid(row=0,column=1)
+instructions.grid(row=1,column=0)
+inputStock.grid(row=1,column=1)
 submitButton.grid(row=1,column=2)
+
 ### Main loop ###
 root.mainloop()
