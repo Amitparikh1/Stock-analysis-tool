0b465c1af774e7121052d69cea9e548c075dabd0
Amit Parikh <amitparikh41@gmail.com>
2020-06-01 13:48:57 -0400

    added image icon

--- GUI.py
+++ GUI.py
@@ -3,26 +3,29 @@
 from tkinter import *
 from Analysis import *
 
+root = Tk() #create root window to put widgets onto
+root.title("Stock Analyzer Tool")
+root.iconbitmap('images\stock.ico')
 ### FUNCTIONS ### 
 def showCurrentPrice():
     tickerSymbol = inputStock.get() #get input text
     price = getCurrentPrice(scrapeData(tickerSymbol)) 
     priceLabel = Label(root,text="Current price of "+tickerSymbol+" is: $"+price)
-    priceLabel.pack()
+    priceLabel.pack(row=2,column=1)
     return
 
-### Basic Setup of Widgets ###
-root = Tk() #create root window to put widgets onto
+### SETUP WIDGETS ###
+
 #labels
 title = Label(root,text="Stock Analyzer")
-instructions = Label(root,text="Enter a Stock:")
+instructions = Label(root,text="Choose a stock:")
 #buttons
 submitButton = Button(root,text="Submit",state=NORMAL,bg="green",fg="white",command = showCurrentPrice)
 #input box
 inputStock = Entry(root) # use .get() to get the text from the input box
 inputStock.insert(0,"Ticker Symbol")
 
-### Put on Screen ###
+### PUT ON SCREEN ###
 title.grid(row=0,column=1)
 instructions.grid(row=1,column=0)
 inputStock.grid(row=1,column=1)
@@ -31,5 +34,5 @@ submitButton.grid(row=1,column=2)
 
 
 
-### Main loop ###
+### MAIN LOOP ###
 root.mainloop()
