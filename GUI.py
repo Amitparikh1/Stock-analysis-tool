47d40ff851f9290967514791f294bb2ef8c3d348
Amit Parikh <amitparikh41@gmail.com>
2020-06-02 16:01:10 -0400

    imported plotting packages and started function to compare two stock prices for inputted time on a graph

--- GUI.py
+++ GUI.py
@@ -2,47 +2,82 @@
 ### Imports ###
 from tkinter import *
 from Analysis import *
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd 
 
 root = Tk() #create root window to put widgets onto
+root.geometry("600x200")
 root.title("Stock Analyzer Tool")
 root.iconbitmap('images\stock.ico')
 ### FUNCTIONS ### 
 def showCurrentPrice():
-    tickerSymbol = inputStock.get() #get input text
+    tickerSymbol = inputStockOne.get() #get input text
     price = getCurrentPrice(scrapeData(tickerSymbol)) 
     priceLabel = Label(root,text="Current price of "+tickerSymbol+" is: $"+price)
     priceLabel.grid(row=2,column=1)
     return
-def showHistoricPrices():
+# def showHistoricPrices():
+#     lengthDesired = inputLengthOne.get()
+#     tickerSymbol = inputStockOne.get() #get input text
+#     historic_prices = getHistoricPrices(scrapeData(tickerSymbol),int(lengthDesired))
+#     historic_prices_string = ""
+#     for price in historic_prices:
+#         historic_prices_string += "$"+price+"  "
+#     priceLabel = Label(root,text="Stock price for the last "+lengthDesired+ " days of "+tickerSymbol+" are: "+historic_prices_string)
+#     priceLabel.grid(row=2,column=1)
+#     return
+def graphHistoricPrices():
     lengthDesired = inputLength.get()
-    tickerSymbol = inputStock.get() #get input text
-    historic_prices = getHistoricPrices(scrapeData(tickerSymbol),int(lengthDesired))
-    historic_prices_string = ""
-    for price in historic_prices:
-        historic_prices_string += "$"+price+"  "
-    priceLabel = Label(root,text="Stock price for the last "+lengthDesired+ " days of "+tickerSymbol+" are: "+historic_prices_string)
-    priceLabel.grid(row=2,column=1)
-    return
+    tickerSymbolOne = inputStockOne.get() #get input text
+    tickerSymbolTwo = inputStockTwo.get()
+    historic_prices_one = getHistoricPrices(scrapeData(tickerSymbolOne),int(lengthDesired))
+    historic_prices_two = getHistoricPrices(scrapeData(tickerSymbolTwo),int(lengthDesired))
+    historic_prices_combined = []
+    stock_type = []
+    days = []
+    for i in range(0,int(lengthDesired)) : 
+        historic_prices_combined.append(float(historic_prices_one[i]))
+        stock_type.append(tickerSymbolOne)
+        days.append(i+1)
+        historic_prices_combined.append(float(historic_prices_two[i]))
+        stock_type.append(tickerSymbolTwo)
+        days.append(i+1)
+    # for price in historic_prices_one:
+    #     historic_int_prices_one.append(float(price))
+    # for price in historic_prices_two:
+    #     historic_int_prices_two.append(float(price))
+    d = {'Day':days, 'Price':historic_prices_combined, 'stock': stock_type }
+    compareDf = pd.DataFrame(data = d)
+    sns.pairplot(x_vars=['Day'],y_vars = ['Price'],data = compareDf, hue='stock',kind='reg')
+    plt.show()
 
 ### SETUP WIDGETS ###
 
 #labels
 title = Label(root,text="Stock Analyzer")
-instructions = Label(root,text="Choose a stock:")
+instructions = Label(root,text="Choose two stocks and the number of days:")
 #buttons
-submitButton = Button(root,text="Submit",state=NORMAL,bg="green",fg="white",command = showHistoricPrices)
-#input boxes
-inputStock = Entry(root) # use .get() to get the text from the input box
-inputStock.insert(0,"Ticker Symbol")
+submitButton = Button(root,text="Submit",state=NORMAL,bg="green",fg="white",command = graphHistoricPrices)
+##input boxes
+#stock 1 
+inputStockOne = Entry(root) # use .get() to get the text from the input box
+inputStockOne.insert(0,"Ticker Symbol")
+#stock 2 
+inputStockTwo = Entry(root) # use .get() to get the text from the input box
+inputStockTwo.insert(0,"Ticker Symbol")
+#length 
 inputLength = Entry(root) # use .get() to get the text from the input box
 inputLength.insert(0,"Length")
 
 ### PUT ON SCREEN ###
 title.grid(row=0,column=1)
 instructions.grid(row=1,column=0)
-inputStock.grid(row=1,column=1)
-inputLength.grid(row=1,column=2)
-submitButton.grid(row=1,column=3)
+inputStockOne.grid(row=1,column=1)
+inputStockTwo.grid(row=1,column=2)
+inputLength.grid(row=2,column=1)
+submitButton.grid(row=2,column=2)
 
 
 
